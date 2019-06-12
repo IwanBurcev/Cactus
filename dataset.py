16394c5216234517e5275a3c013458653b1af471
eclipser <eclipser@wisebits.com>
2019-06-12 17:03:13 +0300

    case for test data

--- dataset.py
+++ dataset.py
@@ -3,28 +3,32 @@ import numpy as np
 
 
 class Dataset():
-    def __init__(self, samples, training=False):
+    def __init__(self, samples, training=False, test=False):
         self.samples = samples
         self.training = training
+        self.test = test
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, item):
-        image, label = self.samples[item]
+        if self.test:
+            image, filename = self.samples[item]
+        else:
+            image, label = self.samples[item]
 
         image = self.normalize(image)
         image = np.transpose(image, axes=[2, 0, 1])
         image = torch.FloatTensor(image)
 
-        return image, label
+        if self.test:
+            return image, filename
+        else:
+            return image, label
 
     @staticmethod
     def normalize(image):
-
         std = np.std(image)
-
         if std > 0:
             image = (image - np.mean(image)) / std
-
         return image
